c69359eb3982f1eda1a6baa0ec59a755121e9e20
Ronaldo <ronaldo.cisneiros@gmail.com>
2016-08-06 20:59:56 -0300

    Versionamento do projeto de exemplo da Aula 01

---
+++ ProjetoAula01/src/OlaMundo.java
@@ -0,0 +1,11 @@
+
+public class OlaMundo {
+
+	/**
+	 * Classe para testes inciais.
+	 * @param args Os argumentos de entrada
+	 */
+	public static void main(String[] args){
+		System.out.println("Ola Mundo");
+	}
+}
