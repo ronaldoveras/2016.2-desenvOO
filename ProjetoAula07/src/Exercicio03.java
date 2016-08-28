241029c4214f85928e33012617285095a9fcbe78
Ronaldo Veras <ronaldo.cisneiros@gmail.com>
2016-08-28 14:30:25 -0300

    Alteração da codificação

--- ProjetoAula07/src/Exercicio03.java
+++ ProjetoAula07/src/Exercicio03.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Exercicio03 {
 
 	public static void main(String[] args) {
-		System.out.println("Digite o primeiro nÃºmero: ");
+		System.out.println("Digite o primeiro número: ");
 		Scanner entradaNumeroUm = new Scanner(System.in);
 		int numeroUm = entradaNumeroUm.nextInt();
 		
 		
-		System.out.println("Digite o segundo nÃºmero: ");
+		System.out.println("Digite o segundo número: ");
 		Scanner entradaNumeroDois = new Scanner(System.in);
 		int numeroDois = entradaNumeroDois.nextInt();
 		
 
-		System.out.println("Digite a operaÃ§Ã£o: ");
+		System.out.println("Digite a operação: ");
 		Scanner entradaOperacao = new Scanner(System.in);
 		String operacao = entradaOperacao.next();
 
