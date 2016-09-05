9886f41b459e2b341e34d89e0f401a56c2d63688
Ronaldo Veras <ronaldo.cisneiros@gmail.com>
2016-09-05 18:31:07 -0300

    Projeto de Introdução as Classes Java

---
+++ ProjetoAula10-IntroOO-Classes/src/NomeClasse.java
@@ -0,0 +1,22 @@
+
+public class NomeClasse {
+
+	//definição dos atributos
+	int atributo1;
+	int atributo2;
+	String atributo3;
+	
+	
+	//Definição dos métodos da classe
+	public void metodo1(){
+		
+	}
+	
+	private int metodo2(int parametro){
+		return parametro;
+	}
+	
+	public void metodo3(int parametro) {
+		metodo2(parametro);
+	}
+}
