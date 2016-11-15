9088cc6d9ec8276ebc3b4d31d55e850b0876d454
Ronaldo Veras <ronaldo.cisneiros@gmail.com>
2016-11-15 16:01:04 -0300

    Alterações realizadas em sala de aula para introduçaõ do Polimorfismo

--- ProjetoAula21-Polimorfismo/src/CarrinhoDeCompras.java
+++ ProjetoAula21-Polimorfismo/src/CarrinhoDeCompras.java
@@ -1,17 +1,32 @@
 public class CarrinhoDeCompras {
 	
 	private double total;
-	
-	public void adiciona(LivroFisico livro) {
+	public int contadorLivroFisico;
+	public int contadorEbook;
+//	public void adiciona(LivroFisico livro) {
+//		System.out.println("Livro Fisico");
+//		System.out.println("Adicionando: " + livro);
+//		total = total + livro.getValor(); 
+//	}
+//
+//	public void adiciona(Ebook livro) {
+//		System.out.println("Ebook");
+//		System.out.println("Adicionando: " + livro);
+//		total += livro.getValor(); 
+//	}
+
+	public void adiciona(Livro livro) {
+		if(livro instanceof LivroFisico){
+			contadorLivroFisico += 1;
+		}
+		if(livro instanceof Ebook){
+			System.out.println(((Ebook)livro).getWaterMark());
+			contadorEbook +=1;
+		}
 		System.out.println("Adicionando: " + livro);
-		total += livro.getValor(); 
+		total = total + livro.getValor(); 
 	}
 
-	public void adiciona(Ebook livro) {
-		System.out.println("Adicionando: " + livro);
-		total += livro.getValor(); 
-	}
-	
 	public double getTotal() {
 		return total;
 	}
--- ProjetoAula21-Polimorfismo/src/RegistroDeVendas.java
+++ ProjetoAula21-Polimorfismo/src/RegistroDeVendas.java
@@ -2,16 +2,24 @@ public class RegistroDeVendas {
 	public static void main(String[] args) {
 		Autor autor = new Autor();
 		autor.setNome("Mauricio Aniche");
-		LivroFisico fisico = new LivroFisico(autor);
+		Livro fisico = new LivroFisico(autor);
 		fisico.setNome("Test-Driven Development");
 		fisico.setValor(59.90);
 		
-		Ebook ebook = new Ebook(autor);
+		Livro ebook = new Ebook(autor);
 		ebook.setNome("Test-Driven Development");
+		((Ebook) ebook).setWaterMark("Marca dágua");
 		ebook.setValor(29.90);
 		
 		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 		carrinho.adiciona(fisico);
+		carrinho.adiciona(fisico);
+		carrinho.adiciona(fisico);
+
 		carrinho.adiciona(ebook);
-		System.out.println("Total " + carrinho.getTotal());	}
+		System.out.println("Total " + carrinho.getTotal());	
+		System.out.println("Total LV: " + carrinho.contadorLivroFisico);
+		System.out.println("Total Ebook: " + carrinho.contadorEbook);
+		
+	}
 }
